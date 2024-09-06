3082752007b18f22caf5a4019b2d4187f422175b
aea555 <akarahmet2002@gmail.com>
2024-09-06 10:33:26 +0300

    -Change the custom entity id validator to allow for null and not required fiels

--- Dto/Injection/UpdateInjection.cs
+++ Dto/Injection/UpdateInjection.cs
@@ -5,11 +5,11 @@ namespace ClinicManagementInternship.Dto.Injection
 {
     public class UpdateInjection : GenericUpdateDTO
     {
-        [ValidEntityId<Models.Patient>("Patients")]
+        [ValidEntityId<Models.Patient>("Patients", isRequired: false)]
         public int? PatientId { get; set; }
-        [ValidEntityId<Models.Doctor>("Doctors")]
+        [ValidEntityId<Models.Doctor>("Doctors", isRequired: false)]
         public int? DoctorId { get; set; }
-        [ValidEntityId<Models.Drug>("Drugs")]
+        [ValidEntityId<Models.Drug>("Drugs", isRequired: false)]
         public int? DrugId { get; set; }
     }
 }
--- Utils/ValidEntityIdAttribute.cs
+++ Utils/ValidEntityIdAttribute.cs
@@ -4,35 +4,38 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ClinicManagementInternship.Utils
 {
-    public class ValidEntityIdAttribute<T> : ValidationAttribute where T : class
+    public class ValidEntityIdAttribute<T>(string entitySetName, bool isRequired = true) : ValidationAttribute where T : class
     {
-        private readonly string _entitySetName;
-
-        public ValidEntityIdAttribute(string entitySetName)
-        {
-            _entitySetName = entitySetName;
-        }
+        private readonly string _entitySetName = entitySetName;
+        private readonly bool _isRequired = isRequired;
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            if (value is null || !(value is int id))
+            // If the value is null and not required, skip validation
+            if (!_isRequired && value == null)
+                return ValidationResult.Success;
+
+            // If value is required but missing or not an integer, return error
+            if (value == null)
             {
-                return new ValidationResult("Id is required and must be an integer.");
+                if (_isRequired)
+                    return new ValidationResult("Id is required.");
+
+                return ValidationResult.Success; // If not required and value is null, skip validation
             }
 
+            // Check if the value is an integer
+            if (value is not int id)
+                return new ValidationResult("Id must be an integer.");
 
             if (validationContext.GetService(typeof(DataContext)) is not DataContext dbContext)
-            {
                 return new ValidationResult("Unable to access the database context. Make sure DbContext is correctly registered.");
-            }
 
             var entitySet = dbContext.Set<T>();
             var entityExists = entitySet.Any(e => EF.Property<int>(e, "Id") == id);
 
             if (!entityExists)
-            {
                 return new ValidationResult($"Entity with Id {id} does not exist.");
-            }
 
             return ValidationResult.Success;
         }
